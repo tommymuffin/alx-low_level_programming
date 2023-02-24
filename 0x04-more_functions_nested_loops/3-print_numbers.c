3290f383d506eecf8b35ef921b844fe3dad89b4d
Tomiwa <tomiwaayanlade@gmail.com>
2023-02-24 03:13:05 -0800

    print 3 numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int q;
+
+	for (q = 48; q < 58; q++)
+	{
+		_putchar(q);
+	}
+	_putchar('\n');
+}
+
