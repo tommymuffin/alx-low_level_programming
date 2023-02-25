bd72ef109e0bc22e99045a1dafde00ad7b0b815f
Tomiwa <tomiwaayanlade@gmail.com>
2023-02-24 16:49:22 -0800

    6 print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
+ * Return: no return.
+ */
+void print_line(int n)
+
+{
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else 
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
