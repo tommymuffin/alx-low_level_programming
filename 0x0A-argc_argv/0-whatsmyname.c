5f7f052ddc1d25805557994551ee14097cb59ded
Tomiwa <tomiwaayanlade@gmail.com>
2023-03-10 18:14:04 -0800

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Prints the number of arguments passed into it.
+* @argc: Numbers of arguments.
+* @argv: Holds the arguments.
+* Return: 0 to succes.
+*/
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
