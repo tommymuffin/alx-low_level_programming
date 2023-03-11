d99cb6f58f2cb9c586e112a88243dcbf15ea2b77
Tomiwa <tomiwaayanlade@gmail.com>
2023-03-10 18:26:26 -0800

    4 add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Sum two numbers.
+* @argc: Numbers of arguments.
+* @argv: Holds the arguments.
+* Return: 0 to succes.
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0, b, i;
+
+	for (b = 1; b < argc; b++)
+	{
+		for (i = 0; argv[b][i]; i++)
+		{
+			if (argv[b][i] < '0' || argv[b][i] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[b]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
