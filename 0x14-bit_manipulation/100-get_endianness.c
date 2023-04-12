bdc2c8031381029da5f28bdef65d310d5484052c
Tomiwa <tomiwaayanlade@gmail.com>
2023-04-11 21:38:46 -0700

    seven

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return:0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int value = 1;
+	char *ptr = (char *)&value;
+
+	if (*ptr == 1)
+		return (1);
+	else
+		return (0);
+}
